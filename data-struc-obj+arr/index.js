7e69a102a19a1de62b8dd502650a92c98b344531
peymanprd <dev.pirzadeh@gmail.com>
2022-01-08 22:12:48 +0330

    starterisfinisher

---
+++ data-struc-obj+arr/index.js
@@ -0,0 +1,26 @@
+/**
+ * @a {start}
+ * @b {end}
+ * @step {addStep}
+ * [return array of start and end with step count]
+ */
+function range(a, b, step) {
+    let arr = [a]
+    if (step && step != 1) while (a + step <= b) arr.push((a += step))
+    else while (a <= b) arr.push((a += 1))
+    return arr
+}
+
+console.log(range(2, 12, 3))
+
+/**
+ * @nums {arrayOfNumbers}
+ * [return sum all items in nums array]
+ */
+function sum(nums) {
+    let sum = 0
+    nums.forEach((i) => (sum += i))
+    return sum
+}
+
+console.log(sum(range(1, 10, 2)))
